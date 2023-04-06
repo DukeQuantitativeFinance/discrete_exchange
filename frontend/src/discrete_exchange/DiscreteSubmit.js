fb6d8806ccd779d6146782076f9795b6995133ce
ben594 <benli20042013@gmail.com>
2023-04-06 14:51:41 -0400

    Basic file submission

--- frontend/src/discrete_exchange/DiscreteSubmit.js
+++ frontend/src/discrete_exchange/DiscreteSubmit.js
@@ -1,15 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function DiscreteSubmit(props) {
-  return (
-    <div>
-      Please submit your trader code below
-      <form>
-        <label>
-            Select .py file 
-        </label>
-        <input type='file' name='trader'/>
-      </form>
+    // see: https://codefrontend.com/file-upload-reactjs/
+    const [file, setFile] = useState();
+
+    function submitFile() {
+        if (file) {
+            console.log("has file");
+            axios.post('http://127.0.0.1:5000/discrete/submit', file, {
+                headers: {
+                  "Content-Type": "multipart/form-data",
+                }
+            });
+        }
+    }
+
+    function changeFile(e) {
+        console.log("file changed");
+        setFile(e.target.files);
+    }
+
+    return (
+        <div>
+            Please submit your trader code below
+        <br />
+        <form>
+            <label>
+                <input type='file' name='trader' accept='.py' onChange={(e) => changeFile(e)}/>
+            </label>
+            <br />
+        </form>
+        <button onClick={submitFile}>
+            Submit
+        </button>
     </div>
   );
 }
